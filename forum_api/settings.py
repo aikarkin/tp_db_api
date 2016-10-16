9774363da261f76738099e1b347994bfe8efb5e3
Karkin Alexandr <karkin.ai@mail.ru>
2016-10-16 13:58:53 +0300

    added README.md, changed path in settings.py

--- forum_api/settings.py
+++ forum_api/settings.py
@@ -1,8 +1,8 @@
 SQL_QUERY_FILES = {
-    'common.clear': '/home/alex/dev/python/forum_api/queries/common/clear.sql',
-    'common.status': '/home/alex/dev/python/forum_api/queries/common/status.sql',
-    'forum.create': '/home/alex/dev/python/forum_api/queries/forum/create.sql',
-    'forum.details': '/home/alex/dev/python/forum_api/queries/forum/details.sql'
+    'common.clear': '/home/alex/dev/python/forum_api/resources/queries/common/clear.sql',
+    'common.status': '/home/alex/dev/python/forum_api/resources/queries/common/status.sql',
+    'forum.create': '/home/alex/dev/python/forum_api/resources/queries/forum/create.sql',
+    'forum.details': '/home/alex/dev/python/forum_api/resources/queries/forum/details.sql'
 }
 
 DB_HOST = 'localhost'
@@ -10,4 +10,4 @@ DB_USER = 'root'
 DB_PASSWORD = 'euqvuips'
 DB_NAME = 'forum'
 DB_TABLES = ['follower', 'forum', 'post', 'subscribe', 'thread', 'user']
-INDEX_HTML = '/home/alex/dev/python/forum_api/index.html'
+INDEX_HTML = '/home/alex/dev/python/forum_api/resources/index.html'
